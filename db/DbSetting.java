9a135d46de68cad34b55350e9399e56314dd9d8e
Alex Nikonov <KarlosRPG@yandex.ru>
2024-10-27 21:32:04 +0300

    Initial commit

---
+++ db/DbSetting.java
@@ -0,0 +1,10 @@
+package ru.netology.db;
+
+public class DbSetting {
+    String name;
+    String password;
+    public DbSetting(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
